ee4779578d5c446ae18493b16c5c8129ebd0a21c
Patric Pauluk <patric.pauluk@outlook.com>
2022-09-06 21:03:57 -0300

    query params e route params

--- first_api/index.js
+++ first_api/index.js
@@ -1,15 +1,31 @@
 const express = require('express') // yarn add express
 
-// verifica a importação
-// console.log(express);
+// Exemplos:
+// query params = ?nome=NodeJS
+// route params = /curso/2
+// request body = { nome: 'NodeJS', tipo: 'Backend' }
 
 const server = express();
 
-// localhost:3000/curso
-server.get('/curso', (req, res) => { // rotas de cursos
-    // console.log('Acessou a rota!');
+
+// --------------- Exemplo de query params ---------------
+// localhost:3000/curso?nome=Javascript
+server.get('/curso', (req, res) => { // rota de curso
     // return res.send('Hello world!'); // exibe no navegador
-    return res.json({ curso: 'Node JS' }); // retorna um json
+
+    const nome = req.query.nome;
+    return res.json({ curso: `Aprendendo ${nome}` }); // retorna um json
 })
+
+
+// --------------- Exemplo de route params ---------------
+// localhost:3000/cursos/1
+server.get('/cursos/:id', (req, res) => { // rota de curso
+    // return res.send('Hello world!'); // exibe no navegador
+
+    const id = req.params.id;
+    return res.json({ curso: `Curso ${id}` }); // retorna um json
+})
+
 
 server.listen(3000); // escuta o servidor na porta 3000
