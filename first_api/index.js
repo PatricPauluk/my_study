c4532df55705ce914db2d8c7e540ed7f0abc4750
Patric Pauluk <patric.pauluk@outlook.com>
2022-09-13 17:13:53 -0300

    requisições route params feitas no insomnia

--- first_api/index.js
+++ first_api/index.js
@@ -1,31 +1,29 @@
 const express = require('express') // yarn add express
 
 // Exemplos:
-// query params = ?nome=NodeJS
-// route params = /curso/2
+// query params = localhost:3000/curso?nome=Javascript
+// route params = localhost:3000/cursos/1
 // request body = { nome: 'NodeJS', tipo: 'Backend' }
 
 const server = express();
 
 
+
 // --------------- Exemplo de query params ---------------
-// localhost:3000/curso?nome=Javascript
 server.get('/curso', (req, res) => { // rota de curso
     // return res.send('Hello world!'); // exibe no navegador
-
     const nome = req.query.nome;
     return res.json({ curso: `Aprendendo ${nome}` }); // retorna um json
 })
 
 
+
 // --------------- Exemplo de route params ---------------
-// localhost:3000/cursos/1
-server.get('/cursos/:id', (req, res) => { // rota de curso
-    // return res.send('Hello world!'); // exibe no navegador
-
-    const id = req.params.id;
-    return res.json({ curso: `Curso ${id}` }); // retorna um json
+const cursos = ['Node JS', 'Javascript', 'React Native'];
+// Inserir no Insomnia:
+server.get('/cursos/:index', (req, res) => { // rota de curso
+    const { index } = req.params; // os colchetes tem relação com ECMAscript
+    return res.json(cursos[index]);
 })
-
 
 server.listen(3000); // escuta o servidor na porta 3000
