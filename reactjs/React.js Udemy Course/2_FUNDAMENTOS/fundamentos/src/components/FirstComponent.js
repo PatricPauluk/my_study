72b8ab624310c56b1301b180451f952c0e2b0635
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-01 16:57:15 -0300

    2_fundamentos - creating components

---
+++ reactjs/React.js Udemy Course/2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+// Meu primeiro componente
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+// Exportação do componente para utilizar fora do código
+export default FirstComponent;
