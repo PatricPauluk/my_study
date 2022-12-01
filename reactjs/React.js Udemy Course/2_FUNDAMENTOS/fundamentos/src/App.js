6eed8123e7aca86264b0b6749bd1cfae731765fd
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-01 17:06:42 -0300

    2_fundamentos - importing components

--- reactjs/React.js Udemy Course/2_FUNDAMENTOS/fundamentos/src/App.js
+++ reactjs/React.js Udemy Course/2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,9 +1,15 @@
+// Components
+import FirstComponent from './components/FirstComponent'; // a extensão .js é omitida (em React)
+
+// Styles / CSS
 import './App.css';
 
+// Os componentes são importados em forma de tag <FirstComponent />
 function App() {
   return (
     <div className="App">
       <h1>Olá</h1>
+      <FirstComponent />
     </div>
   );
 }
