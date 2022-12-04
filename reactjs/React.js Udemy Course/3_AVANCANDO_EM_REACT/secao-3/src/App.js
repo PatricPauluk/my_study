99d14a262bc27a69318be83e9ed1d44461c889a9
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-03 22:22:42 -0300

    using images in react

--- reactjs/React.js Udemy Course/3_AVANCANDO_EM_REACT/secao-3/src/App.js
+++ reactjs/React.js Udemy Course/3_AVANCANDO_EM_REACT/secao-3/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+import Asset from "./assets/img2.jpg";
+
+// As imagens do projeto podem ficar ou na pasta public, ou na pasta src/assets.
+
 function App() {
   return (
     <div className="App">
-      <h1>Seção 3</h1>
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        {/* A pasta public é linkada com a pasta src, então podem ser chamadas diretamente na tag. */}
+        <img id="imagem" src="/img1.jpg" alt="Imagem de public" />
+      </div>
+      {/* Imagem em Assets */}
+      <div>
+        {/* A pasta assets NÃO É é linkada com a pasta src, então é importada. */}
+        <img id="imagem" src={Asset} alt="Imagem de assets" />
+      </div>
     </div>
   );
 }
