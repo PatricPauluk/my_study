fd969a18bb4c7462e2fbfb2c476caf9b36c985d7
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-07 21:47:55 -0300

    conditional render if/else

--- reactjs/React.js Udemy Course/3_AVANCANDO_EM_REACT/secao-3/src/components/ConditionalRender.js
+++ reactjs/React.js Udemy Course/3_AVANCANDO_EM_REACT/secao-3/src/components/ConditionalRender.js
@@ -4,13 +4,25 @@ import { useState } from "react";
 const ConditionalRender = () => {
 
   const [x, setX] = useState(true);
+  const [name, setName] = useState();
 
   return (
     <div>
+
+      {/* Simples situações que um conteúdo é exibido ou oculto */}
+
       <h2>Renderização condicional</h2>
       <button onClick={() => setX(false)}>Parágrafo não exibe</button>
       <button onClick={() => setX(true)}>Parágrafo exibe</button>
       {x && <p>Estou sendo exibido</p>}
+      
+      
+      <button onClick={() => setName("Patric")}>Confirma o nome Patric</button>
+      {name === "Patric" ? (
+        <p>O nome é Patric</p>
+      ) : (
+        <p>Nome não encontrado</p>
+      )}
     </div>
   );
 }
