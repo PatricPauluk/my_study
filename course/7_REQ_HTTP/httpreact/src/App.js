3704ff896f226b9b265139f5147d4da9761cf44d
Patric Pauluk <patric.pauluk@outlook.com>
2023-01-17 22:10:46 -0300

    fetch tests

--- course/7_REQ_HTTP/httpreact/src/App.js
+++ course/7_REQ_HTTP/httpreact/src/App.js
@@ -1,6 +1,32 @@
 import './App.css';
 
+import { useState, useEffect } from "react";
+
+// url da API
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [ products, setProducts ] = useState([]);
+
+  // resgatando dados do banco
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      // fetch é uma alternativa nativa para conexão (como o Axios)
+      const res = await fetch(url);
+  
+      // converte dados de json para objeto para ser utilizado
+      const data = await res.json();
+  
+      setProducts(data);
+    }
+
+    fetchData()
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
